3cb76797353dfdb84ce2322a531cf652ad683dcb
l08084 <l08084.1989@gmail.com>
2020-01-06 20:24:29 +0900

    first commit

---
+++ a-welcom-to-atcoder.cpp
@@ -0,0 +1,16 @@
+// A - Welcome to AtCoder
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    string s;
+
+    cin >> a;
+    cin >> b >> c;
+    cin >> s;
+
+    cout << a + b + c << " " << s << "\n";
+}
