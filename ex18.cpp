afd0870fd7c997cac8e5d0dfb27a0b324a85871b
l08084 <l08084.1989@gmail.com>
2020-03-15 22:44:03 +0900

    ex18 ac

--- ex18.cpp
+++ ex18.cpp
@@ -15,24 +15,32 @@ int main()
     // (ここで"試合結果の表"の2次元配列を宣言)
 
     vector<vector<char>> result(N, vector<char>(N));
-
-    for (int i = 0; i < M; i++)
+    for (int i = 0; i < N; i++)
     {
         for (int j = 0; j < N; j++)
         {
-            if (A.at(i) == result.at(i).at(i))
+            result.at(i).at(j) = '-';
+
+            for (int k = 0; k < M; k++)
             {
-                cout << 'o';
+                if (A[k] == i + 1 && B[k] == j + 1)
+                {
+                    result.at(i).at(j) = 'o';
+                }
+                else if (A[k] == j + 1 && B[k] == i + 1)
+                {
+                    result.at(i).at(j) = 'x';
+                }
             }
-            else if (B.at(i) == result.at(i).at(i))
-            {
-                cout << 'x';
-            }
-            else
-            {
-                cout << '-';
-            }
-            if (j == N)
+        }
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            cout << result.at(i).at(j);
+            if (j == (N - 1))
             {
                 cout << endl;
             }
