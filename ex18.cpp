b057622cc85d6f3cfe8648cc4fbce29e0df8f538
l08084 <l08084.1989@gmail.com>
2020-03-15 20:39:02 +0900

    update

--- ex18.cpp
+++ ex18.cpp
@@ -14,7 +14,8 @@ int main()
     // ここにプログラムを追記
     // (ここで"試合結果の表"の2次元配列を宣言)
 
-    vector<vector<char>> result(M, vector<char>(N));
+    vector<vector<char>> result(N, vector<char>(N));
+
     for (int i = 0; i < M; i++)
     {
         for (int j = 0; j < N; j++)
