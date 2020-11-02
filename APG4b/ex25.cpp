f3c0606198017bcfaf8e160b59e16bc0b1d749e8
l08084 <l08084.1989@gmail.com>
2020-11-02 19:22:49 +0900

    更新

--- APG4b/ex25.cpp
+++ APG4b/ex25.cpp
@@ -6,26 +6,32 @@ using namespace std;
 // AとBに共通して含まれる要素からなる集合を返す
 bitset<50> intersection(bitset<50> A, bitset<50> B)
 {
+    return A & B;
 }
 // AとBのうち少なくとも一方に含まれる要素からなる集合を返す
 bitset<50> union_set(bitset<50> A, bitset<50> B)
 {
+    return A | B;
 }
 // AとBのうちどちらか一方にだけ含まれる要素からなる集合を返す
 bitset<50> symmetric_diff(bitset<50> A, bitset<50> B)
 {
+    return A ^ B;
 }
 // Aから値xを除く
 bitset<50> subtract(bitset<50> A, int x)
 {
+    return A.reset(x);
 }
 // Aに含まれる要素に1を加える(ただし、要素49が含まれる場合は0になるものとする)
 bitset<50> increment(bitset<50> A)
 {
+    return A << 1;
 }
 // Aに含まれる要素から1を引く(ただし、要素0が含まれる場合は49になるものとする)
 bitset<50> decrement(bitset<50> A)
 {
+    return A >> 1;
 }
 
 // Sに値xを加える
